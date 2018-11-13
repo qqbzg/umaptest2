8a31f570653392bf27a24b10f92a67a023b260f2
liuxiyuan <liuxiyuan@abchina.com>
2018-11-13 14:47:46 +0800

    ddd

---
+++ sayHello.framework/Headers/sayHello.h
@@ -0,0 +1,19 @@
+//
+//  sayHello.h
+//  sayHello
+//
+//  Created by liuxiyuan on 2018/11/12.
+//  Copyright © 2018 liuxiyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sayHello.
+FOUNDATION_EXPORT double sayHelloVersionNumber;
+
+//! Project version string for sayHello.
+FOUNDATION_EXPORT const unsigned char sayHelloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sayHello/PublicHeader.h>
+
+#import <sayHello/sayHi.h>
---
+++ sayHello.framework/Headers/sayHi.h
@@ -0,0 +1,19 @@
+//
+//  sayHello.h
+//  sayHello
+//
+//  Created by liuxiyuan on 2018/11/12.
+//  Copyright © 2018 liuxiyuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface sayHi : NSObject
+
+- (void)sayHi;
+
+@end
+
+NS_ASSUME_NONNULL_END
